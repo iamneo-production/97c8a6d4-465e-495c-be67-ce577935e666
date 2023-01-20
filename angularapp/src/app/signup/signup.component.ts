a6529b083ac8e905aefb3bea5fc0955f7df6fd59
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-20 09:51:51 +0000

    4th commit

--- angularapp/src/app/signup/signup.component.ts
+++ angularapp/src/app/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
