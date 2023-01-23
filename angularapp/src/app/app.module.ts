07a084ba6685a254e3f260cf83b4cc323ed69234
MatabaneThabang <thabang.matabane@lntinfotech.com>
2023-01-23 09:47:09 +0000

    committed.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,32 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-
-import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AuthComponent } from './auth/auth.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { LoginComponent } from './auth/login/login.component';
-import { DisplayUserComponent } from './auth/display-user/display-user.component';
-import { SearchComponent } from './auth/search/search.component';
-import { EditUserComponent } from './auth/edit-user/edit-user.component';
-import { DeleteUserComponent } from './auth/delete-user/delete-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AuthComponent,
-    SignupComponent,
-    LoginComponent,
-    DisplayUserComponent,
-    SearchComponent,
-    EditUserComponent,
-    DeleteUserComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule, ReactiveFormsModule, HttpClientModule, NgbModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
