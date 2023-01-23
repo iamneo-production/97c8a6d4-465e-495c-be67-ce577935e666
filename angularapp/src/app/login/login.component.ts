9884bc2feb121bfc2e025d0cb9b9b2d3ca76347a
MatabaneThabang <thabang.matabane@lntinfotech.com>
2023-01-23 11:26:49 +0000

    commit

--- angularapp/src/app/login/login.component.ts
+++ angularapp/src/app/login/login.component.ts
@@ -1,28 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,Validators } from '@angular/forms';
-import { ServiceService } from '../services/service.service';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
+  found : boolean = false;
+  constructor(private login:LoginService){}
 
-  users:any;
-  constructor(private userdata: ServiceService)
-  {
-    userdata.users().subscribe((data)=>
-    {
-      console.log(data);
-      this.users=data;
-    }
-    );
-  }
-
-    // constructor(private userdata:ServiceService){}
-
-  loginform = new FormGroup(
+  loginForm = new FormGroup(
     {
       email: new FormControl('',[Validators.required,Validators.email]),
       password: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(12), Validators.pattern('[a-z A-Z 1-9]+$')]),
@@ -30,25 +19,28 @@ export class LoginComponent implements OnInit {
     });
     get email()
     {
-      return this.loginform.get('email');
+      return this.loginForm.get('email');
     }
 
     get password()
     {
-      return this.loginform.get('password');
+      return this.loginForm.get('password');
     }
-    loginUser()
+    loginUser(user:any)
     {
-      // this.userdata.users().subscribe((user:any)=>{
-      //     if(user.email === this.loginform.get('email')){
-      //         console.log("Logged In");
-      //     }
-      // });
-    console.warn(this.loginform.value);
+        this.login.getUsers().subscribe((users:any)=>{
+          for(let c = 0; c < users.length; c++){
+            if(users[c].username == user.username == users[c].password == user.password){
+              this.found = true;
+            }
+          }
+        });
+
+        if(this.found) alert("Logged In");
+        else alert("Not Successful");
     }
 
 
-  ngOnInit(): void {
-  }
+
 
 }
