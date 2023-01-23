50f5089215bf43af9bf2fcfa5c321ea87f9dbe6c
AlbinahHlongo <albinankateko@gmail.com>
2023-01-23 06:08:21 +0000

    Gift's changes

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,33 +3,21 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { UserDisplayComponent } from './user-display/user-display.component';
+import { SignupComponent } from './signup/signup.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
+    SignupComponent,
     UserDisplayComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
-    
-=======
-import { ReactiveFormsModule } from '@angular/forms';
-import { SignupComponent } from './signup/signup.component';
-import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
-@NgModule({
-  declarations: [
-    AppComponent, SignupComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule, ReactiveFormsModule, HttpClientModule, NgbModule
->>>>>>> 9e08a6970ae3cd563afff00ddf94d747e100bfae
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
