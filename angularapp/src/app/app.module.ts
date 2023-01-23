ab46e99aad10e265fd08008eec828f654b0635d4
MatabaneThabang <thabang.matabane@lntinfotech.com>
2023-01-23 10:01:26 +0000

    stashed.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LoginComponent
   ],
   imports: [
     BrowserModule,
