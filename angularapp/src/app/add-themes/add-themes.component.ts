c8b80670eee2434ea4d5940b92393a78f566f1dc
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-23 04:27:30 +0000

    commit addTheme

--- angularapp/src/app/add-themes/add-themes.component.ts
+++ angularapp/src/app/add-themes/add-themes.component.ts
@@ -1,16 +1,22 @@
 import { Component} from '@angular/core';
-import { FormGroup, FormControl} from '@angular/forms';
+import { ThemeserviceService} from '../services/themeservice.service';
 
 @Component({
   selector: 'app-add-themes',
   templateUrl: './add-themes.component.html',
   styleUrls: ['./add-themes.component.css']
 })
-export class AddThemesComponent implements OnInit {
+export class AddThemesComponent {
+themes:any;
+themdata:any;
+  constructor(themedata:ThemeserviceService) {
+    themedata.themes().subscribe((data:any)=>
+    {
+      console.log(data);
+      this.themes=data;
+    })
+   }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+ 
 
 }
