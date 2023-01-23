7c4d6bcbd90d7af1d78887dc750f7a477f5da238
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-23 10:05:41 +0000

    Routings

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -1,13 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddThemesComponent } from './add-themes/add-themes.component';
+import { LoginComponent } from './auth/login/login.component';
 
-import { SignupComponent } from './signup/signup.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const routes: Routes = [
 
   {
     path:"signup",
     component:SignupComponent
+  },
+  {
+    path:"login",
+    component:LoginComponent
+  },
+  {
+   path:"addTheme",
+   component:AddThemesComponent
   }
 
 ];
