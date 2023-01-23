a06f7ad44a3c4422e90b693f8bc5dfe5543fa493
MatabaneThabang <thabang.matabane@lntinfotech.com>
2023-01-23 08:28:27 +0000

    commit.

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -4,14 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { SignupComponent } from './signup/signup.component';
-import { LoginComponent } from './login/login.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AuthComponent } from './auth/auth.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent, SignupComponent, LoginComponent
+    AppComponent,
+    AuthComponent,
+    SignupComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
