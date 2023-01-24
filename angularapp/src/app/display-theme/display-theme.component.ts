c9b8c8edc1e17a2de3bb34c5d944c49c8eeca30c
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-24 11:54:53 +0000

    A

--- angularapp/src/app/display-theme/display-theme.component.ts
+++ angularapp/src/app/display-theme/display-theme.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ThemedisplayService } from '../services/themedisplay.service';
 
 @Component({
   selector: 'app-display-theme',
   templateUrl: './display-theme.component.html',
   styleUrls: ['./display-theme.component.css']
 })
-export class DisplayThemeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class DisplayThemeComponent  {
+   display=true;
+   theTheme:any;
+   them:any =[];
+   themExists:boolean = false;
+  constructor(private displayThemeService:ThemedisplayService) { 
+    this.displayThemeService.getTheme().subscribe((data:any)=>
+    {
+      this.theTheme = data;
+      console.log(data);
+    });
   }
 
+
+  
 }
