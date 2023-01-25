81312c42c684380c5d9343b523dbee832c95df9f
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-25 13:20:55 +0000

    A

--- angularapp/src/app/admin/admin.component.spec.ts
+++ angularapp/src/app/admin/admin.component.spec.ts
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-<<<<<<<< HEAD:angularapp/src/app/admin/admin.component.spec.ts
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { AdminComponent } from './admin.component';
-
-describe('AdminComponent', () => {
-  let component: AdminComponent;
-  let fixture: ComponentFixture<AdminComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AdminComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
-========
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { SignupComponent } from './signup.component';
-
-describe('SignupComponent', () => {
-  let component: SignupComponent;
-  beforeEach(()=>TestBed.configureTestingModule({
-    imports:[HttpClientTestingModule],
-    providers:[SignupComponent]
-  }))
-  beforeEach(() => {
-    const fixture = TestBed.createComponent(SignupComponent);
-    component = fixture.componentInstance;
-  });
-  it('FE_SignupTest', () => {
-    expect(component).toBeTruthy();
-  });
-});
->>>>>>>> 1844be9596195eb4572a620ae75c591eb818aac9:angularapp/src/app/auth/signup/signup.component.spec.ts
-=======
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 // import { RouterTestingModule } from '@angular/router/testing';
 import { RouterTestingModule } from '@angular/router/testing';
@@ -67,4 +18,3 @@ describe('AddproductComponent', () => {
     expect(component).toBeTruthy();
   });
 });
->>>>>>> 1844be9596195eb4572a620ae75c591eb818aac9
