4c9a776266c0ecb2dd283ff5e5cb2c25bd1ea833
AlbinahHlongo <albinankateko@gmail.com>
2023-01-25 07:46:24 +0000

    displaying users

--- angularapp/src/app/auth/display-user/display-user.component.ts
+++ angularapp/src/app/auth/display-user/display-user.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { UserdisplayService } from 'src/app/services/userdisplay.service';
 
 interface UserDetails
@@ -10,10 +9,6 @@ interface UserDetails
 }
 
 
-=======
-import { UserDisplayService } from 'src/app/services/user-display.service';
->>>>>>> 1844be9596195eb4572a620ae75c591eb818aac9
-
 @Component({
   selector: 'app-display-user',
   templateUrl: './display-user.component.html',
@@ -21,25 +16,12 @@ import { UserDisplayService } from 'src/app/services/user-display.service';
 })
 export class DisplayUserComponent implements OnInit {
 
-<<<<<<< HEAD
-  searchUser:any;
-  users:any;
-
-  constructor(private userdisplay:UserdisplayService) 
-  { 
-    userdisplay.users().subscribe((data:any) =>
-    {
-      console.log(data);
-      this.users=data;
-    })
-=======
   users: any;
   
-  constructor(private userService:UserDisplayService){
+  constructor(private userService:UserdisplayService){
     this.userService.users().subscribe((data:any)=>{
       this.users = data;
   });
->>>>>>> 1844be9596195eb4572a620ae75c591eb818aac9
   }
 
   
@@ -47,11 +29,11 @@ export class DisplayUserComponent implements OnInit {
   }
 
 
-  getUserFormData(data:any)
-  {
-    this.userdisplay.saveUsers(data).subscribe((result:any) =>
-    {
-      console.log(result);
-    })
-  }
+  // getUserFormData(data:any)
+  // {
+  //   this.userdisplay.saveUsers(data).subscribe((result:any) =>
+  //   {
+  //     console.log(result);
+  //   })
+  // }
 }
