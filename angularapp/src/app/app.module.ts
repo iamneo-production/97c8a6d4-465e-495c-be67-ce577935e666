3457f3a7d6c01db42d403069a8d093dd156a5cdf
GiftMakhuv <gift.makhuvele@lntinfotech.com>
2023-01-25 06:57:14 +0000

    deleted files that won't be needed

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -3,13 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { LoginComponent } from './login/login.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClientModule } from '@angular/common/http';
-import { DisplaythemeComponent } from './displaytheme/displaytheme.component';
-=======
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -21,22 +14,10 @@ import { DisplayUserComponent } from './auth/display-user/display-user.component
 import { SearchComponent } from './auth/search/search.component';
 import { EditUserComponent } from './auth/edit-user/edit-user.component';
 import { DeleteUserComponent } from './auth/delete-user/delete-user.component';
->>>>>>> 3d357351bf7abfb36a661702019fbff6d56da5bb
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginComponent,
-    DisplaythemeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    NgbModule,
-    ReactiveFormsModule,
-    HttpClientModule
-=======
     AuthComponent,
     SignupComponent,
     LoginComponent,
@@ -48,7 +29,6 @@ import { DeleteUserComponent } from './auth/delete-user/delete-user.component';
   imports: [
     BrowserModule,
     AppRoutingModule, ReactiveFormsModule, HttpClientModule, NgbModule
->>>>>>> 3d357351bf7abfb36a661702019fbff6d56da5bb
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angularapp/src/app/auth/login/login.component.ts
+++ angularapp/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup,FormControl,Validators } from '@angular/forms';
+import { ServiceService } from 'src/app/services/service.service';
+
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  users:any;
+  constructor(private userdata: ServiceService)
+  {
+    userdata.users().subscribe((data)=>
+    {
+      console.log(data);
+      this.users=data;
+    }
+    );
+  }
+
+    // constructor(private userdata:ServiceService){}
+
+  loginform = new FormGroup(
+    {
+      email: new FormControl('',[Validators.required,Validators.email]),
+      password: new FormControl('',[Validators.required,Validators.minLength(5),Validators.maxLength(12), Validators.pattern('[a-z A-Z 1-9]+$')]),
+
+    });
+    get email()
+    {
+      return this.loginform.get('email');
+    }
+
+    get password()
+    {
+      return this.loginform.get('password');
+    }
+    loginUser()
+    {
+      // this.userdata.users().subscribe((user:any)=>{
+      //     if(user.email === this.loginform.get('email')){
+      //         console.log("Logged In");
+      //     }
+      // });
+    console.warn(this.loginform.value);
+    }
+
 
   ngOnInit(): void {
   }
