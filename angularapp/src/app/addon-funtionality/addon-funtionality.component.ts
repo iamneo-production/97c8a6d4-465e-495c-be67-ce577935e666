2e8e475e536dc7ecd6b132dde81cd2956cf50815
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-28 13:27:38 +0000

    A

--- angularapp/src/app/addon-funtionality/addon-funtionality.component.ts
+++ angularapp/src/app/addon-funtionality/addon-funtionality.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-addon-funtionality',
   templateUrl: './addon-funtionality.component.html',
   styleUrls: ['./addon-funtionality.component.css']
 })
 export class AddonFuntionalityComponent implements OnInit {
+  constructor(config: NgbModalConfig, private modalService: NgbModal) {
+		// customize default values of modals used by this component tree
+		config.backdrop = 'static';
+		config.keyboard = false;
+	}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+	open(content:any) {
+		this.modalService.open(content);
+	}
 
 }
