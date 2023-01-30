c61e9000fb18babfd50bcba9a284eb81f045e737
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-30 13:22:45 +0000

    A

--- angularapp/src/app/addtheme/addtheme.component.ts
+++ angularapp/src/app/addtheme/addtheme.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-addtheme',
   templateUrl: './addtheme.component.html',
   styleUrls: ['./addtheme.component.css']
 })
-export class AddthemeComponent  {
+export class AddthemeComponent implements OnInit {
 
+  constructor() { }
 
+  ngOnInit(): void {
+  }
 
 }
