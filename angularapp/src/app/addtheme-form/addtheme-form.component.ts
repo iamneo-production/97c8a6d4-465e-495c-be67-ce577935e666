065258d786ddbe162f06e8be3489b7b2f9a10a16
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-30 11:02:56 +0000

    Model Driven Form

--- angularapp/src/app/addtheme-form/addtheme-form.component.ts
+++ angularapp/src/app/addtheme-form/addtheme-form.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import {ThemeserviceService } from '../services/themeservice.service';
 @Component({
-  selector: 'app-addtheme-form',
+  selector: '',
   templateUrl: './addtheme-form.component.html',
   styleUrls: ['./addtheme-form.component.css']
 })
