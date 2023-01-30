ad5bdd4ad921ee6adc2f148584898992728f9622
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-30 14:53:11 +0000

    A

--- angularapp/src/app/addtheme/addtheme.component.ts
+++ angularapp/src/app/addtheme/addtheme.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {ThemeserviceService } from '../services/themeservice.service';
 @Component({
   selector: 'app-addtheme',
   templateUrl: './addtheme.component.html',
   styleUrls: ['./addtheme.component.css']
 })
-export class AddthemeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class AddthemeComponent  {
+  themeForm= new FormGroup(
+    {
+      themeName:new FormControl('',[Validators.required]),
+      imageUrl:new FormControl('',[Validators.required]),
+      photographerDetails:new FormControl('',[Validators.required]),
+      videographerDetails:new FormControl('',[Validators.required]),
+      returnGift:new FormControl('',[Validators.required]),
+      themeCost:new FormControl('',[Validators.required]),
+      themeCostDescription:new FormControl('',[Validators.required])
+    }
+  );
+  get ThemeName(){
+    return this.themeForm.get('themeName');
   }
+  get imageUrl(){
+    return this.themeForm.get('imageUrl');
+  }
+  get Photographer(){
+    return this.themeForm.get('photographerDetails');
+  }
+  get videographerDetails(){
+    return this.themeForm.get('videographerDetails');
+  }
+  get returnGift(){
+    return this.themeForm.get('returnGift');
+  }
+  get themeCost(){
+    return this.themeForm.get('themeCost');
+  }
+  get themeDescription(){
+    return this.themeForm.get('themeDescription');
+  }
+  constructor(private themedata:ThemeserviceService) { }
+  
+  addThemeData(data:any){
+    this.themedata.addtheme(data).subscribe((result:any)=>
+    {
+       console.log(result);
+       alert("Theme Added Successfully!!!");
+    });
+    
+  }
+
 
 }
