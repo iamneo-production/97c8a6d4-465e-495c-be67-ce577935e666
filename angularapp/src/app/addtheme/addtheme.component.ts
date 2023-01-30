3531e535ea8430c1b48bc09ce58ffc4e0a071139
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-30 13:13:15 +0000

    A

--- angularapp/src/app/addtheme/addtheme.component.ts
+++ angularapp/src/app/addtheme/addtheme.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-addtheme',
@@ -8,14 +8,6 @@ import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AddthemeComponent  {
 
-  constructor(config: NgbModalConfig, private modalService: NgbModal) {
-		// customize default values of modals used by this component tree
-		config.backdrop = 'static';
-		config.keyboard = false;
-	}
 
-	open(content:any) {
-		this.modalService.open(content);
-	}
 
 }
