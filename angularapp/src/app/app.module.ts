dfe1d6aa19169994aa602b1584c867b2500d5f25
esihletuswa <Esihle.Tuswa@lntinfotech.com>
2023-01-31 06:57:20 +0000

    A

--- angularapp/src/app/app.module.ts
+++ angularapp/src/app/app.module.ts
@@ -33,7 +33,7 @@ import { CustomerheaderComponent } from './customerheader/customerheader.compone
 import { AdminheaderComponent } from './adminheader/adminheader.component';
 import { EditThemeComponent } from './edit-theme/edit-theme.component';
 import { EditMenuComponent } from './edit-menu/edit-menu.component';
-import { ViewAddOnsComponent } from './view-add-ons/view-add-ons.component';
+
 
 
 
@@ -46,7 +46,7 @@ import { ViewAddOnsComponent } from './view-add-ons/view-add-ons.component';
     HomepageComponent,ViewmenuComponent,DeletethemeComponent,
     ViewthemeComponent,DeletemenuComponent, ReviewsComponent, 
     AddthemeComponent, DeleteAddonComponent, EditAddonComponent, EditBookingComponent, FooterComponent,
-    CustomerheaderComponent, AdminheaderComponent, EditThemeComponent, EditMenuComponent, ViewAddOnsComponent
+    CustomerheaderComponent, AdminheaderComponent, EditThemeComponent, EditMenuComponent
   ],
   imports: [
     BrowserModule,
